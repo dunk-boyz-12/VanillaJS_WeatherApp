b53842d7293a567cdbd14ef4ed5517345bcafe54
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2020-12-08 16:19:50 -0800

    oh yeah baby

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import { callApi } from './utility/callWeatherDB';
 import { degreeConv } from './utility/degConv'; 
 import { WeatherCard } from './utility/weatherCard';
 
-const dayContainer = document.querySelector(".weather_card_container");
-const forecastStateContainer = document.getElementById("header_weather_display_container");
-const citySearch= document.getElementById("form_search");
-const degreeContainer = document.getElementById("header__conv__container");
+const cards = document.querySelectorAll(".weather-card");
+const forecastStateContainer = document.getElementById("header-weather-display-container");
+const citySearch= document.getElementById("form-search");
+const degreeContainer = document.getElementById("header-conv-container");
 const forecastAmt = [1,3];
 let forecastState = forecastAmt[1];
 let usingFaranheit = true;
-let weather;
+let weather; //will hold weather data returned from api after formatting
 
 //event listeners
 citySearch.addEventListener('submit', function(e) {
@@ -38,7 +38,6 @@ const newSearch = (e) => {
     // here,, call function to check input 
     callApi(city,forecastState)
         .then(data => formatData(data))
-        .then(() => console.log(weather))
         .then(() => displayData(weather));
 };
 
@@ -47,12 +46,8 @@ const validateUserInput = () => {
 };
 //just for test, need to fix
 const displayData = (data) => {
-    let cards = data.Forecast.map(day => {
-        let card = document.createElement('p');
-        card.innerText = day.date;
-        return card;
-    });
-    cards.forEach( c => dayContainer.appendChild(c));
+    //cards has weather cards stored in it to add data to and cycle
+    console.log(data);
 };
 
 const formatData = (data) => {
@@ -86,75 +81,8 @@ const formatData = (data) => {
                 Alert: weatherAlert, 
                 Current: currentData, 
                 Forecast: [...formattedData] };
-    //console.log(weather);
+
 };
 
 
 export { usingFaranheit };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-makeDay.prototype.createDay = function() {
-    let card = document.createElement('div');
-    let day = document.createElement('h2');
-    let month = document.createElement('h2');
-    let iconContainer = document.createElement('div');
-    let icon = document.createElement('ion-icon');
-    let infoContainer = document.createElement('div');
-    let condition = document.createElement('p');
-    let tHigh = document.createElement('p');
-    let tLow = document.createElement('p');
-
-    card.className = 'weather_card';
-    day.className = 'weather_card__day';
-    month.className = 'weather_card__month';
-    iconContainer.id = 'weather_card__icon__container';
-    icon.className = 'weather_card__icon';
-    infoContainer.className = 'weather_card__info__container';
-    condition.className = 'weather_card__current';
-    tHigh.className = 'weather_card__item';
-    tLow.className = 'weather_card__item';
-    day.innerText = this.dateDay;
-    month.innerText = this.dateMonth_Num;
-    icon.name = this.icon;
-    condition.innerText = this.condition;
-    tHigh.innerText = `H ${this.tHigh} F`;
-    tLow.innerText = `H ${this.tLow} C`;
-
-    card.appendChild(day);
-    card.appendChild(month);
-    iconContainer.appendChild(icon);
-    card.appendChild(iconContainer);
-    infoContainer.appendChild(condition);
-    infoContainer.appendChild(tHigh);
-    infoContainer.appendChild(tLow);
-    card.appendChild(infoContainer);
-    dayContainer.appendChild(card);
-}
-
-const initWeek = function() {
-    for(let i = 0; i < 7; i++) {
-        let d = new makeDay();
-        d.createDay();
-        days.push(d);
-    }
-}
-
-
-//initWeek();
-console.log(days);
-degreeContainer.addEventListener('click', degreeConv);*/
